a5d6bf44aa38da6eb7f7f69ac5c0b28a519e98e8
ferry <1359453864@qq.com>
2023-10-11 00:33:18 +0800

    add new map

--- script.js
+++ script.js
@@ -1,98 +1,6 @@
-// // const inputArea = document.getElementById('inputArea');
-// // const outputArea = document.getElementById('outputArea');
-// // const map = {
-// //     '0': 't',
-// //     '1': 'd',
-// //     '2': 'f',
-// //     '3': 'w',
-// //     '4': 'k',
-// //     '5': 'p',
-// //     '6': 'a',
-// //     '7': 'y',
-// //     '8': 'm',
-// //     '9': 'e'
-// // };
-
-// // inputArea.addEventListener('input', (e) => {
-// //     let value = e.target.value.replace(/\s+/g, '');
-
-// //     if (/^[0-9]+$/.test(value)) {
-// //         let result = value.split('').map(num => map[num]).join('');
-// //         result = result.charAt(0).toUpperCase() + result.slice(1);
-// //         outputArea.value = result;
-// //     } else if (/^[a-zA-Z]+$/.test(value)) {
-// //         let result = Object.entries(map).find(([key, val]) => val === value.toLowerCase())?.[0] || '';
-// //         outputArea.value = result;
-// //     } else {
-// //         outputArea.value = '';
-// //     }
-
-// //     copyToClipboard(outputArea.value);
-// // });
-
-// // function copyToClipboard(text) {
-// //     const textarea = document.createElement('textarea');
-// //     textarea.value = text;
-// //     document.body.appendChild(textarea);
-// //     textarea.select();
-// //     document.execCommand('copy');
-// //     document.body.removeChild(textarea);
-// // }
-
-// const inputArea = document.getElementById("inputArea");
-// const outputArea = document.getElementById("outputArea");
-// const map = {
-//   0: "t",
-//   1: "d",
-//   2: "f",
-//   3: "w",
-//   4: "k",
-//   5: "p",
-//   6: "a",
-//   7: "y",
-//   8: "m",
-//   9: "e",
-// };
-
-// inputArea.addEventListener("input", (e) => {
-//   let value = e.target.value.replace(/\s+/g, "");
-
-//   if (/^[0-9]+$/.test(value)) {
-//     let result = value
-//       .split("")
-//       .map((num) => map[num])
-//       .join("");
-//     result = result.charAt(0).toUpperCase() + result.slice(1);
-//     outputArea.value = result;
-//   } else if (/^[a-z]+$/i.test(value)) {
-//     let result = value
-//       .split("")
-//       .map((char) => {
-//         for (let [num, mappedChar] of Object.entries(map)) {
-//           if (mappedChar === char.toLowerCase()) return num;
-//         }
-//         return char;
-//       })
-//       .join("");
-//     outputArea.value = result;
-//   } else {
-//     outputArea.value = "";
-//   }
-
-//   copyToClipboard(outputArea.value);
-// });
-
-// function copyToClipboard(text) {
-//   const textarea = document.createElement("textarea");
-//   textarea.value = text;
-//   document.body.appendChild(textarea);
-//   textarea.select();
-//   document.execCommand("copy");
-//   document.body.removeChild(textarea);
-// }
-
 const inputArea = document.getElementById('inputArea');
 const outputArea = document.getElementById('outputArea');
+const numberProcessed = document.getElementById('numberProcessed'); // 新增的区域
 const copyAlert = document.getElementById('copyAlert');
 const map = {
   0: "t",
@@ -105,15 +13,17 @@ const map = {
   7: "y",
   8: "m",
   9: "e",
+  z: "+"  // 新的映射规则
 };
 
 inputArea.addEventListener("input", (e) => {
   let value = e.target.value.replace(/\s+/g, "");
+  numberProcessed.value = value; // 显示处理后的数字
 
-  if (/^[0-9]+$/.test(value)) {
+  if (/^[0-9+zZ]+$/.test(value)) {
     let result = value
       .split("")
-      .map((num) => map[num])
+      .map((num) => map[num] || num) // 使用新的映射规则
       .join("");
     result = result.charAt(0).toUpperCase() + result.slice(1);
     outputArea.value = result;
@@ -133,7 +43,6 @@ inputArea.addEventListener("input", (e) => {
   }
 
   copyToClipboard(outputArea.value);
-  // Return focus to the input area after copying
   inputArea.focus();
   showCopyAlert();
 });
@@ -142,8 +51,9 @@ function showCopyAlert() {
     copyAlert.classList.add('show');
     setTimeout(() => {
         copyAlert.classList.remove('show');
-    }, 1000);  // Alert shows for 2 seconds
+    }, 1000);  
 }
+
 function copyToClipboard(text) {
   const textarea = document.createElement("textarea");
   textarea.value = text;
@@ -151,6 +61,4 @@ function copyToClipboard(text) {
   textarea.select();
   document.execCommand("copy");
   document.body.removeChild(textarea);
-  // Add a simple copy alert (this can be replaced with a nicer UI element if desired)
-  // alert('内容已复制到剪贴板!');
 }
